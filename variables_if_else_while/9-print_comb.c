492755fc8cebf967daf07c8a4b97fc6273d35f2d
Aziz94tg <aziz94tg@gmail.com>
2025-02-13 15:46:37 +0000

    A

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+	
+	/* Loop throgh numbers 0-9 */
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0'); 
+	
+		/* Print comma and space for all except the last number */
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	/* Print newline */
+	putchar('\n');
+
+	return (0);
+}
