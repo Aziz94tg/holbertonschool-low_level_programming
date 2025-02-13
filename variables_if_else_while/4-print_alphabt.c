6d94cbcca42349d07300a266af251ba99a881416
Aziz94tg <aziz94tg@gmail.com>
2025-02-13 15:57:59 +0000

    A

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print all the letters except q and e
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	char letter;
+
+	/* Loop through lowercase letters from 'a' to 'z' */
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
+	/* Print newline */
+	putchar('\n');
+
+	return(0);
+}
