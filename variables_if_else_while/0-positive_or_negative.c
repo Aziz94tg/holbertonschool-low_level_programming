5264216a69d4bafc821248a470e348bc2b5ea846
Aziz94tg <aziz94tg@gmail.com>
2025-02-13 13:51:11 +0000

    A

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int n;
 
-       	/* Random number generatior */
+/* Random number generatior */
 	srand(time(0));
-       	n = rand() - RAND_MAX / 2;
-	
-	/* Check if the number is positive, negative, or zero */
+	n = rand() - RAND_MAX / 2;
+
+/* Check if the number is positive, negative, or zero */
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
