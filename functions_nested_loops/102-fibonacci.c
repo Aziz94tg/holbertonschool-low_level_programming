eaacf43751fd65f413f8f3d966be0289660e08c8
Aziz94tg <aziz94tg@gmail.com>
2025-03-03 20:25:14 +0000

    A

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 
 	printf("%ld, %ld", a, b); /* Print first two numbers */
 
-	for (count = 3; count <= 50; count++) /* Loop from 3rd to 50th Fibonacci number */
+	for (count = 3; count <= 50; count++) /* Loop from 3rd to 50th Fibonacci */
 	{
 		next = a + b; /* Compute next Fibonacci number */
 		printf(", %ld", next);
