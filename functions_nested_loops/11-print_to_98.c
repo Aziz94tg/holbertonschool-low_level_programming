c8853468ec0d149830a312a3c53080a8eecec99b
Aziz94tg <aziz94tg@gmail.com>
2025-03-03 19:03:31 +0000

    A

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,23 +7,24 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98) /* Count up to 98 */
-    {
-        for (; n <= 98; n++)
-        {
-            printf("%d", n);
-            if (n != 98) /* Print comma and space if not last number */
-                printf(", ");
-        }
-    }
-    else /* Count down to 98 */
-    {
-        for (; n >= 98; n--)
-        {
-            printf("%d", n);
-            if (n != 98) /* Print comma and space if not last number */
-                printf(", ");
-        }
-    }
-    printf("\n"); /* Print newline at the end */
+	if (n <= 98) /* Count up to 98 */
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98) /* Print comma and space if not last number */
+				printf(", ");
+		}
+	}
+	else /* Count down to 98 */
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98) /* Print comma and space if not last number */
+				printf(", ");
+		}
+	}
+	printf("\n"); /* Print newline at the end */
 }
+
