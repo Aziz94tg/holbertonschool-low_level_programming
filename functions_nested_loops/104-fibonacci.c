3ea78b6735f954abb3bc9b8dc65ce78bc8f619db
Aziz94tg <aziz94tg@gmail.com>
2025-03-03 20:34:35 +0000

    A

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -31,28 +31,26 @@ void print_fibonacci_large(unsigned long int a, unsigned long int b)
 	unsigned long int a_high, a_low, b_high, b_low, next_high, next_low;
 	int count;
 
-	a_high = a / 1000000000;
-	a_low = a % 1000000000;
+	a_high = a / 1000000000; /* Extract high part */
+	a_low = a % 1000000000;  /* Extract low part */
 	b_high = b / 1000000000;
 	b_low = b % 1000000000;
 
 	for (count = 91; count <= 98; count++)
 	{
-		next_high = a_high + b_high;
-		next_low = a_low + b_low;
+		next_high = a_high + b_high; /* Compute high part */
+		next_low = a_low + b_low;     /* Compute low part */
 
-		if (next_low >= 1000000000)
+		if (next_low >= 1000000000) /* Handle overflow in low part */
 		{
 			next_high += 1;
 			next_low -= 1000000000;
 		}
 
-		/* Fix leading zero issue */
-		if (next_high == 0) /* If high part is 0, print only low part */
-			printf(", %lu", next_low);
-		else /* Otherwise, print both high and low parts */
-			printf(", %lu%09lu", next_high, next_low);
+		/* Correctly print the Fibonacci number */
+		printf(", %lu%09lu", next_high, next_low);
 
+		/* Update previous numbers */
 		a_high = b_high;
 		a_low = b_low;
 		b_high = next_high;
@@ -70,7 +68,7 @@ int main(void)
 	unsigned long int a = 1, b = 2;
 
 	print_fibonacci_normal(a, b); /* Print first 90 numbers */
-	print_fibonacci_large(a, b);  /* Print large numbers */
+	print_fibonacci_large(a, b);  /* Print large numbers correctly */
 
 	printf("\n");
 	return (0);
