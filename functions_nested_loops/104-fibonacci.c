0de6178f8302782a4dffa7afe8a0eb09efa77402
Aziz94tg <aziz94tg@gmail.com>
2025-03-03 20:43:11 +0000

    A

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-    unsigned long a = 1, b = 2, next;
+    unsigned long a1 = 0, a2 = 1, b1 = 0, b2 = 2, next1, next2;
     int i;
 
-    printf("%lu, %lu", a, b);
+    printf("%lu, %lu, %lu, %lu", a2, b2, a2 + b2, a2 + 2 * b2);
 
-    for (i = 2; i < 98; i++) {
-        next = a + b;
-        printf(", %lu", next);
-        a = b;
-        b = next;
+    for (i = 4; i < 98; i++) {
+        next1 = a1 + b1;
+        next2 = a2 + b2;
+        if (next2 < a2 || (next2 == a2 && next1 < a1)) {
+            next1++;
+        }
+        printf(", %lu", next2);
+        a1 = b1;
+        a2 = b2;
+        b1 = next1;
+        b2 = next2;
     }
     printf("\n");
     return 0;
