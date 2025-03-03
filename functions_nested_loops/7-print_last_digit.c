6fb58390a5020c5fce31dde336dc5a29a3d998fc
Aziz94tg <aziz94tg@gmail.com>
2025-03-03 18:31:58 +0000

    A

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,11 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
+	last_digit = n % 10;  /* Get last digit */
+	if (last_digit < 0)   /* Ensure it's positive */
+		last_digit = -last_digit;
 
-    last_digit = n % 10;  /* Get last digit */
-    if (last_digit < 0)   /* Ensure it's positive */
-        last_digit = -last_digit;
-
-    _putchar('0' + last_digit); /* Print the digit */
-    return (last_digit);
+	_putchar('0' + last_digit); /* Print the digit */
+	return (last_digit);
 }
