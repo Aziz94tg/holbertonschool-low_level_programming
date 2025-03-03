f7dbe8e457d12de0085d7680e2b49756b0662197
Aziz94tg <aziz94tg@gmail.com>
2025-03-03 20:30:41 +0000

    A

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers
- *
- * Return: Always 0 (Success)
+ * print_fibonacci_normal - Prints Fibonacci numbers from 1 to 90
+ * @a: First number
+ * @b: Second number
  */
-int main(void)
+void print_fibonacci_normal(unsigned long int a, unsigned long int b)
 {
-	unsigned long int a = 1, b = 2, next;
-	unsigned long int a_high, a_low, b_high, b_low, next_high, next_low;
+	unsigned long int next;
 	int count;
 
-	/* Print first two Fibonacci numbers */
 	printf("%lu, %lu", a, b);
 
-	/* Print Fibonacci numbers up to F(90) using normal addition */
 	for (count = 3; count <= 90; count++)
 	{
 		next = a + b;
@@ -22,37 +19,55 @@ int main(void)
 		a = b;
 		b = next;
 	}
+}
 
-	/* Split numbers into high and low parts (handling large numbers) */
-	a_high = a / 1000000000; /* Extract high part */
-	a_low = a % 1000000000;  /* Extract low part */
+/**
+ * print_fibonacci_large - Prints Fibonacci numbers from 91 to 98
+ * @a: First large number
+ * @b: Second large number
+ */
+void print_fibonacci_large(unsigned long int a, unsigned long int b)
+{
+	unsigned long int a_high, a_low, b_high, b_low, next_high, next_low;
+	int count;
+
+	a_high = a / 1000000000;
+	a_low = a % 1000000000;
 	b_high = b / 1000000000;
 	b_low = b % 1000000000;
 
-	/* Continue Fibonacci sequence from F(91) to F(98) */
-	for (; count <= 98; count++)
+	for (count = 91; count <= 98; count++)
 	{
-		next_high = a_high + b_high; /* Compute high part */
-		next_low = a_low + b_low;     /* Compute low part */
+		next_high = a_high + b_high;
+		next_low = a_low + b_low;
 
-		/* Handle overflow in the low part */
 		if (next_low >= 1000000000)
 		{
-			next_high += 1; /* Carry over to high part */
+			next_high += 1;
 			next_low -= 1000000000;
 		}
 
-		/* Print Fibonacci number */
 		printf(", %lu%09lu", next_high, next_low);
-
-		/* Update previous numbers */
 		a_high = b_high;
 		a_low = b_low;
 		b_high = next_high;
 		b_low = next_low;
 	}
+}
 
-	printf("\n"); /* Print newline */
+/**
+ * main - Prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int a = 1, b = 2;
+
+	print_fibonacci_normal(a, b); /* Print first 90 numbers */
+	print_fibonacci_large(a, b);  /* Print large numbers */
+
+	printf("\n");
 	return (0);
 }
 
