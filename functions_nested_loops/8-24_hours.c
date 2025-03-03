c4cb922ab382e5fa76da064c00059232294327bd
Aziz94tg <aziz94tg@gmail.com>
2025-03-03 18:41:00 +0000

    A

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,18 +5,19 @@
  */
 void jack_bauer(void)
 {
-    int hour, min;
+	int hour, min;
 
-    for (hour = 0; hour < 24; hour++)  /* Loop through hours */
-    {
-	    for (min = 0; min < 60; min++)  /* Loop through minutes */
-	    {
-		    _putchar('0' + (hour / 10));  /* Print tens digit of hour */
-		    _putchar('0' + (hour % 10));  /* Print ones digit of hour */
-		    _putchar(':');                /* Print ':' */
-		    _putchar('0' + (min / 10));   /* Print tens digit of minute */
-		    _putchar('0' + (min % 10));   /* Print ones digit of minute */
-		    _putchar('\n');               /* Print newline */
-	    }
-    }
+	for (hour = 0; hour < 24; hour++) /* Loop through hours */
+	{
+		for (min = 0; min < 60; min++) /* Loop through minutes */
+		{
+			_putchar('0' + (hour / 10)); /* Print tens digit of hour */
+			_putchar('0' + (hour % 10)); /* Print ones digit of hour */
+			_putchar(':'); /* Print ':' */
+			_putchar('0' + (min / 10)); /* Print tens digit of minute */
+			_putchar('0' + (min % 10)); /* Print ones digit of minute */
+			_putchar('\n'); /* Print newline */
+		}
+	}
 }
+
