aaac06fd30b4a33db0d223570964e623eda1ee36
Aziz94tg <aziz94tg@gmail.com>
2025-03-07 20:49:56 +0000

    A

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -8,33 +8,33 @@
  */
 int main(void)
 {
-    long number = 612852475143;
-    long largest_factor = 0;
-    long i;
+	long number = 612852475143;
+	long largest_factor = 0;
+	long i;
 
-    /* Step 1: Remove all factors of 2 */
-    while (number % 2 == 0)
-    {
-        largest_factor = 2;
-        number /= 2;
-    }
+	/* Step 1: Remove all factors of 2 */
+	while (number % 2 == 0)
+	{
+		largest_factor = 2;
+		number /= 2;
+	}
 
-    /* Step 2: Remove all odd factors */
-    for (i = 3; i <= sqrt(number); i += 2)
-    {
-        while (number % i == 0)
-        {
-            largest_factor = i;
-            number /= i;
-        }
-    }
+	/* Step 2: Remove all odd factors */
+	for (i = 3; i <= sqrt(number); i += 2)
+	{
+		while (number % i == 0)
+		{
+			largest_factor = i;
+			number /= i;
+		}
+	}
 
-    /* Step 3: If number > 2, it's a prime factor */
-    if (number > 2)
-        largest_factor = number;
+	/* Step 3: If number > 2, it's a prime factor */
+	if (number > 2)
+		largest_factor = number;
 
-    /* Step 4: Print the largest prime factor */
-    printf("%ld\n", largest_factor);
-    return (0);
+	/* Step 4: Print the largest prime factor */
+	printf("%ld\n", largest_factor);
+	return (0);
 }
 
