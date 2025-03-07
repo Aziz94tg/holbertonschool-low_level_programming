5be0da8a12d3746e5862b7513c27127144e53a49
Aziz94tg <aziz94tg@gmail.com>
2025-03-07 20:00:22 +0000

    A

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 to 14, ten times.
+ */
+void more_numbers(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++) /* Repeat 10 times */
+    {
+        for (j = 0; j <= 14; j++) /* Print numbers 0 to 14 */
+        {
+            if (j > 9) /* Print two-digit numbers */
+                _putchar('1');  /* Print '1' for numbers 10-14 */
+            
+            _putchar((j % 10) + '0'); /* Print the last digit */
+        }
+        _putchar('\n'); /* New line after each sequence */
+    }
+}
+
