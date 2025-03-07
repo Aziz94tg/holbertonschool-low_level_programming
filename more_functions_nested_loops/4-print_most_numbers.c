edd4ed3bb13c72a5ea1555be3a23acf588b688b9
Aziz94tg <aziz94tg@gmail.com>
2025-03-07 19:51:32 +0000

    A

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
+ * print_most_numbers - Prints numbers from 0 to 9, except 2 and 4.
  */
 void print_most_numbers(void)
 {
-    char numbers[] = "01356789"; /* Store valid numbers in a string */
-    int i = 0;
+	char numbers[] = "01356789"; /* Store valid numbers in a string */
+	int i = 0;
 
-    while (numbers[i] != '\0') /* Loop through the string */
-    {
-        _putchar(numbers[i]);
-        i++;
-    }
-    _putchar('\n'); /* Print newline */
+	while (numbers[i] != '\0') /* Loop through the string */
+	{
+		_putchar(numbers[i]);
+		i++;
+	}
+	_putchar('\n'); /* Print newline */
 }
-                                                                                                                                                          
+
