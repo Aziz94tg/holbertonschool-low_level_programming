149f25a7d92aaa34c42f993d5f8dc684a3e5f691
Aziz94tg <aziz94tg@gmail.com>
2025-03-11 20:45:39 +0000

    A

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <limits.h> /* For INT_MAX and INT_MIN */
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer safely.
@@ -9,36 +9,35 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int num = 0;
-    int found_digit = 0;
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+	int found_digit = 0;
 
-    while (s[i] != '\0') /* Loop through the string */
-    {
-        if (s[i] == '-')  /* If '-' is found, flip the sign */
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9') /* If a digit is found */
-        {
-            /* Check if multiplying by 10 or adding a new digit will cause overflow */
-            if (num > (INT_MAX / 10) || (num == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
-            {
-                /* If overflow happens, return the max or min int */
-                if (sign == 1)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			/* Prevent overflow before it happens */
+			if (num > (INT_MAX / 10) ||
+			    (num == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+			num = (num * 10) + (s[i] - '0');
+			found_digit = 1;
+		}
+		else if (found_digit)
+			break;
 
-            num = (num * 10) + (s[i] - '0'); /* Convert character to number */
-            found_digit = 1; /* Mark that a number has started */
-        }
-        else if (found_digit) /* Stop when a non-digit appears after a number */
-            break;
+		i++;
+	}
 
-        i++;
-    }
-
-    return (num * sign);
+	return (num * sign);
 }
 
