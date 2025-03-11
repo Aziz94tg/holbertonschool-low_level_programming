a1e2a517d97f3c4e575cc94ea3bc1627d2455631
Aziz94tg <aziz94tg@gmail.com>
2025-03-11 20:44:40 +0000

    A

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,44 @@
 #include "main.h"
+#include <limits.h> /* For INT_MAX and INT_MIN */
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * _atoi - Converts a string to an integer safely.
+ * @s: The input string.
  *
  * Return: The integer value from the string.
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int num = 0;
-	int found_digit = 0;
+    int i = 0;
+    int sign = 1;
+    int num = 0;
+    int found_digit = 0;
 
-	while (s[i] != '\0') /* Go through the string */
-	{
-		/* If we find a minus sign, flip the sign */
-		if (s[i] == '-')
-			sign *= -1;
-		/* If we find a plus sign, do nothing (just keep moving) */
-		else if (s[i] == '+')
-			sign *= 1;
-		/* If we find a digit, start building the number */
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			num = (num * 10) + (s[i] - '0');
-			found_digit = 1; /* Mark that we've found a number */
-		}
-		/* If we've already started forming a number and hit a non-digit, stop */
-		else if (found_digit)
-			break;
+    while (s[i] != '\0') /* Loop through the string */
+    {
+        if (s[i] == '-')  /* If '-' is found, flip the sign */
+            sign *= -1;
+        else if (s[i] >= '0' && s[i] <= '9') /* If a digit is found */
+        {
+            /* Check if multiplying by 10 or adding a new digit will cause overflow */
+            if (num > (INT_MAX / 10) || (num == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
+            {
+                /* If overflow happens, return the max or min int */
+                if (sign == 1)
+                    return INT_MAX;
+                else
+                    return INT_MIN;
+            }
 
-		i++;
-	}
+            num = (num * 10) + (s[i] - '0'); /* Convert character to number */
+            found_digit = 1; /* Mark that a number has started */
+        }
+        else if (found_digit) /* Stop when a non-digit appears after a number */
+            break;
 
-	/* Multiply by sign to get the correct positive/negative number */
-	return (num * sign);
+        i++;
+    }
+
+    return (num * sign);
 }
 
