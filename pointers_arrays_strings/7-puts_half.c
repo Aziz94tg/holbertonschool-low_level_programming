fbfc7e5b5d5fa04e514cc32667943fcc60291b37
Aziz94tg <aziz94tg@gmail.com>
2025-03-11 19:29:52 +0000

    A

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - Print the second half of a string
+ * @str: The string to be printed 
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length = 0, start, i;
+
+	/* Find the length of the string */
+	while (str[length] != '\0')
+		length++;
+
+	/* Calculate the string index for the second half */
+	if (length % 2 == 0)
+	{
+		start = length /2;
+	}
+	else 
+	{
+		start = (length + 1) / 2;
+	}
+
+	/* Print the charcaters from the start index */
+	for (i = start; i < length; i++)
+		_putchar(str[i]);
+
+	_putchar('\n'); 
+}
