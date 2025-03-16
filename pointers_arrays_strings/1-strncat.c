5003ef9adcff105be76bcb93b050124b7a772efa
Aziz94tg <aziz94tg@gmail.com>
2025-03-16 19:15:19 +0000

    A

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings, using at most n bytes from src
- * @dest: The destination string (where src will be appended)
- * @src: The source string to append to dest
- * @n: The max number of bytes to copy from src
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to use from src
  *
- * Return: Pointer to dest (the modified string)
+ * Return: A pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0; /* Keep track of the length of dest */
-    int i = 0; /* Counter for src */
+	int dest_len = 0; /* Variable to track the length of dest */
+	int i = 0; /* Index for iterating through src */
 
-    /* Find the end of dest (where null terminator is) */
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
+	/* Find the end of dest */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
 
-    /* Copy up to n characters from src to dest */
-    while (i < n && src[i] != '\0')
-    {
-        dest[dest_len] = src[i];
-        dest_len++;
-        i++;
-    }
+	/* Append up to n characters from src */
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
 
-    /* Make sure dest is null-terminated */
-    dest[dest_len] = '\0';
+	/* Null-terminate the result */
+	dest[dest_len] = '\0';
 
-    return dest;
+	return (dest);
 }
 
