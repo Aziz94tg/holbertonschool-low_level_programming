fd0a089276727c851cd0ca1430a1e7b914287232
Aziz94tg <aziz94tg@gmail.com>
2025-03-19 21:07:10 +0000

    A

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,6 @@ void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *rot13(char *s);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif /* MAIN_H */
 
