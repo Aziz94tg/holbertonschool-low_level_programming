8540b757ad7e75cf8f2075f276eb24aef5ac5ed8
Aziz94tg <aziz94tg@gmail.com>
2025-04-08 13:39:00 +0000

    A

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,18 +1,19 @@
+; 100-hello_world.asm
 section .data
-    msg     db  "Hello, World", 10     ; The message + newline character
-    msg_len equ $ - msg                ; Length of the message
+    msg db "Hello, World", 10 ; The message + newline
+    len equ $ - msg          ; Calculate message length
 
 section .text
     global _start
 
 _start:
-    mov rax, 1          ; syscall number for write (1)
-    mov rdi, 1          ; file descriptor 1 = stdout
-    mov rsi, msg        ; pointer to message
-    mov rdx, msg_len    ; length of message
-    syscall             ; invoke the system call
+    mov rax, 1       ; syscall number for write
+    mov rdi, 1       ; file descriptor 1 (stdout)
+    mov rsi, msg     ; address of the string
+    mov rdx, len     ; number of bytes
+    syscall          ; make syscall
 
-    mov rax, 60         ; syscall number for exit (60)
-    xor rdi, rdi        ; status 0
-    syscall             ; exit(0)
+    mov rax, 60      ; syscall number for exit
+    xor rdi, rdi     ; exit status 0
+    syscall
 
