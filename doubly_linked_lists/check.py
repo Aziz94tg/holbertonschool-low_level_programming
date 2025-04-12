6eae5335fad0e4aa63cb313f8e3206c09b438339
Aziz94tg <aziz94tg@gmail.com>
2025-04-12 14:08:01 +0000

    A

--- doubly_linked_lists/check.py
+++ doubly_linked_lists/check.py
@@ -1,8 +1,13 @@
-pwd = input("Password: ")
 ok = "Zen of Python"
 ok = ok + " C"
 ok = ok[1:3] + ok[3] + ok[14] + ok[3] + ok[7:9] + ok[:6][::-1]
-if pwd == ok:
+
+print(f"Expected password: '{ok}'")
+entered = input("Password: ")
+
+print(f"Entered password: '{entered}'")
+
+if entered == ok:
     print("OK")
 else:
     print("KO")
